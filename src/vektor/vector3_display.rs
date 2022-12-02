a449d0552528fe71abf5945fb4b3ae91098f63d3
krissmedt <kristoffer@smedt.dk>
2022-12-02 18:48:46 +0000

    Add Vector3 Display trait test

--- src/vektor/vector3_display.rs
+++ src/vektor/vector3_display.rs
@@ -1,6 +1,41 @@
 use std::fmt::{Display, Formatter};
 use crate::vektor::vector3::Vector3;
 
+impl Display for Vector3<i8> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
+
+impl Display for Vector3<i16> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
+
+impl Display for Vector3<i32> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
+
+impl Display for Vector3<i64> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
+
+impl Display for Vector3<i128> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
+
+impl Display for Vector3<isize> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}, {}]", self.x, self.y, self.z)
+    }
+}
 
 impl Display for Vector3<f32> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
--- src/vektor/vector3_display_test.rs
+++ src/vektor/vector3_display_test.rs
@@ -14,15 +14,25 @@ mod vector3_display_test {
         let operand_f32 = Vector3 {x: 1.1 as f32, y: 2.2 as f32, z: 3.3 as f32};
         let operand_f64 = Vector3 {x: 1.1 as f64, y: 2.2 as f64, z: 3.3 as f64};
 
-        let expected_i8 = "[1, 2, 3]";
-        let expected_i16 = Vector3 {x: 2 as i16, y: 4 as i16, z: 6 as i16};
-        let expected_i32 = Vector3 {x: 2 as i32, y: 4 as i32, z: 6 as i32};
-        let expected_i64 = Vector3 {x: 2 as i64, y: 4 as i64, z: 6 as i64};
-        let expected_i128 = Vector3 {x: 2 as i128, y: 4 as i128, z: 6 as i128};
-        let expected_isize = Vector3 {x: 2 as isize, y: 4 as isize, z: 6 as isize};
-        let expected_f32 = Vector3 {x: 2.2 as f32, y: 4.4 as f32, z: 6.6 as f32};
-        let expected_f64 = Vector3 {x: 2.2 as f64, y: 4.4 as f64, z: 6.6 as f64};
+        let expected_integer = "[1, 2, 3]";
+        let expected_float = "[1.1, 2.2, 3.3]";
 
-        let result_i8 = "{}";
+        let result_i8 = format!("{}", operand_i8);
+        let result_i16 = format!("{}", operand_i16);
+        let result_i32 = format!("{}", operand_i32);
+        let result_i64 = format!("{}", operand_i64);
+        let result_i128 = format!("{}", operand_i128);
+        let result_isize = format!("{}", operand_isize);
+        let result_f32 = format!("{}", operand_f32);
+        let result_f64 = format!("{}", operand_f64);
+
+        assert_eq!(result_i8, expected_integer);
+        assert_eq!(result_i16, expected_integer);
+        assert_eq!(result_i32, expected_integer);
+        assert_eq!(result_i64, expected_integer);
+        assert_eq!(result_i128, expected_integer);
+        assert_eq!(result_isize, expected_integer);
+        assert_eq!(result_f32, expected_float);
+        assert_eq!(result_f64, expected_float);
     }
 }
